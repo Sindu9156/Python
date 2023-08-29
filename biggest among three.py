2c825a4e74001fb24984aa83eb0a6533ca6bec0f
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2023-08-29 23:07:12 +0530

    biggest among three.py

---
+++ biggest among three.py
@@ -0,0 +1,11 @@
+
+a=int(input("enter the value"))
+b=int(input("enter the value"))
+c=int(input("enter the value"))
+if(a>b):
+	if(b>c):
+		print("biggest number is",a)
+elif(b>c):
+	print("biggest number is",b)
+else:
+	print(" biggest number is",c)
