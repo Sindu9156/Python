f7c3592b84ab5514f3e1d10f8d9229fb7a4f03ca
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2023-10-24 08:37:56 +0530

    if condition.py

---
+++ if condition.py
@@ -0,0 +1,9 @@
+a=int(input("\n Enter the number1:"))
+b=int(input("\n Enter the number2:"))
+c=int(input("\n Enter the number3:"))
+if(a==b):
+    if(b==c):
+       print("\n the value is",3*(a+b+c));
+else:
+    print("\n don't calculate ");
+
