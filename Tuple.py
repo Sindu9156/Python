36f8b3d963f3f7ba3ad4a035f98953ed1c085094
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2023-11-06 23:06:47 +0530

    Tuple.py

---
+++ Tuple.py
@@ -0,0 +1,10 @@
+tuple1=(1,2,5,7,96,84)
+print(tuple1)
+print(tuple1.count(1))
+print(tuple1.index(96))
+tuple2=tuple(("apple","banana"))
+tuple1+=tuple2
+print(tuple1)
+print(tuple1[2])
+print(tuple1[-2])
+print(tuple1[2:5])
