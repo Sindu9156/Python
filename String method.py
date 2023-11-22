b80df8bca5d6d0f6f37c908a30f99b5eb399808d
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2023-11-22 17:36:51 +0530

    String method.py

---
+++ String method.py
@@ -0,0 +1,21 @@
+s=input() 
+if s.isalnum():
+    print("True") 
+else:
+    print("False") 
+if s.isalpha():
+    print("True") 
+else:
+    print("False")
+if s.isdigit():
+    print("True") 
+else:
+    print("False") 
+if s.islower():
+    print("True") 
+else:
+    print("False")
+if s.isupper():
+    print("True") 
+else:
+    print("False") 
