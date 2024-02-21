0360574c1b59c8cd3cb8409c897d2261934bdf47
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2024-02-21 20:40:43 +0530

    Print the highest value by summing the values of list.py

---
+++ Print the highest value by summing the values of list.py
@@ -0,0 +1,9 @@
+n=int(input("enter the total values")) 
+l=[] 
+print("enter the values") 
+for i in range(n):
+    l.append(int(input())
+l.sort()
+s="" 
+for i in range(n-1,-1,-1) :
+     s=s+l[i]
