f14f7ac60bed29dc6790f4ab8cf61f8555eb0bd8
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2024-02-27 23:46:40 +0530

    Intersection.py

---
+++ Intersection.py
@@ -0,0 +1,11 @@
+s1=set() 
+s2=set() 
+n=int(input("enter the total value for set1")) 
+print("enter the elements for set 1") 
+for i in range(n):
+    s1.add(int(input())) 
+n=int(input("enter the total value for set 2"))
+print("enter the elements for set 1") 
+for i in range(n):
+    s1.add(int(input())) 
+print("the intersection of two set is", s1.intersection_update(s2))
