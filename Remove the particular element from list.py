7356cd40dce1df498bd42ea107bf8d0ce9479a38
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2024-03-07 20:04:46 +0530

    Remove the particular element from list.py

---
+++ Remove the particular element from list.py
@@ -0,0 +1,8 @@
+n=int(input("enter the total values")) 
+l=[] 
+print("enter the values") 
+for i in range(n):
+    l.append(int(input())
+p=int(input("enter the value to delete")) 
+
+print(l.remove(p)) 
